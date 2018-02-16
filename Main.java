673475d165c5698180434055c4222c2835292c51
Unknown <luismiguel92_@hotmail.com>
2018-02-15 21:19:07 -0500

    programa inicial

---
+++ Main.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+
+public class Main{
+
+          public static void main(String[] arg){
+
+                   Scanner num = new Scanner (System.in);
+                   int digito1;
+                   int digito2;
+
+                   System.out.println("Ejercicio 1 ");
+                   System.out.println("");
+                   System.out.println(" Este programa le ayudara a determinar si un numero es mayor menor o igual que otro");
+
+
+                       System.out.println("Digite el primer numero");
+                       digito1 = num.nextInt();
+                   
+                       System.out.println("Digite el segundo numero");
+                       digito2 = num.nextInt();
+                           
+                           if(digito1 == digito2){
+
+                               System.out.println(" Ambos numeros son iguales ")  ;
+                           
+                           }
+                            
+                           if (digito1 > digito2){
+
+                                    
+                                   System.out.println("El numero mayor es el"+digito1 ) ;
+
+
+                           }  
+                             if(digito2 > digito1) {
+
+                                   System.out.println("El numero mayor es el"+digito2)  ;
+
+                            }
+
+                          
+  
+
+
+           }
+
+}
